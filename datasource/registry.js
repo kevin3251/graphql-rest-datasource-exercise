9a9ed2176f3ffe4ae7d2daa2933203c0c1d570a4
kevin3251 <herry3251@gmail.com>
2019-05-06 18:15:33 +0800

    update registry.js

--- datasource/registry.js
+++ datasource/registry.js
@@ -3,6 +3,14 @@ const { RESTDataSource } = require('apollo-datasource-rest')
 class Registry extends RESTDataSource {
     constructor() {
         super()
-        this.baseURL = 'https://registry.npmjs.org'
+        this.baseURL = 'https://registry.npmjs.org/'
+    }
+
+    async getPackage(packageName) {
+        return this.get(`${packageName}`)
+    }
+
+    async getPackageByVersion(packageName, version) {
+        
     }
 }
