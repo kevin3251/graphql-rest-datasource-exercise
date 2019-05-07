b1f28055845536cac94f69eef8b778567ced750b
kevin3251 <herry3251@gmail.com>
2019-05-07 17:56:46 +0800

    add datasource

--- datasource/registry.js
+++ datasource/registry.js
@@ -1,16 +1,25 @@
-const { RESTDataSource } = require('apollo-datasource-rest')
-
-class Registry extends RESTDataSource {
-    constructor() {
-        super()
-        this.baseURL = 'https://registry.npmjs.org/'
-    }
+const axios = require('axios')
+const registry = {
+    baseURL: 'https://registry.npmjs.org/',
 
     async getPackage(packageName) {
-        return this.get(`${packageName}`)
-    }
+        let resp = await axios.get(`${this.baseURL}/${packageName}`)
+        return resp.data
+    },
 
     async getPackageByVersion(packageName, version) {
-        
+        let resp = await axios.get(`${this.baseURL}/${packageName}/${version}`)
+        return resp.data
+    },
+
+    async getSearchResult({
+        text, size, from, quality, popularity, maintenance
+    } = {}) {
+        const url = `${this.baseURL}/text=${text}&size=${size}&from=${from}&quality=${quality}&popularity=${popularity}&maintenanc=${maintenance}`
+        let resp = await axios.get(url)
+        return resp.data
     }
 }
+
+module.exports = registry
+
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const { ApolloServer, gql } = require('apollo-server')
+const registry = require('./datasource/registry')
+const { ApolloServer } = require('apollo-server')
+const { schema } = require('./graphql')
 
+const server = new ApolloServer({
+    schema,
+    context: { registry }
+})
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+})
